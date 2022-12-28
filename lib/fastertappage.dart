146913079bae9339664146ce5ae98fb42dc68497
cc <charan@gmail.com>
2022-12-28 20:29:50 +0530

    made changes to height

--- lib/fastertappage.dart
+++ lib/fastertappage.dart
@@ -2,39 +2,45 @@ import 'package:faster_tap/main.dart';
 import 'package:flutter/material.dart';
 
 class FasterTapPage extends State<FasterTapState> {
-  double x_ratio = 100;
-  double y_ratio = 100;
   int count = 0;
+  double height = 0;
+  double xRatio = 0;
+  double yRatio = 0;
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    // x_ratio = height / 2;
-    // y_ratio = height / 2;
+    setState(() {
+      height = MediaQuery.of(context).size.height;
+      xRatio = height / 2;
+      yRatio = height / 2;
+    });
     return Scaffold(
       body: Column(
         children: [
           GestureDetector(
             onTap: () {
               setState(() {
-                x_ratio++;
-                y_ratio--;
+                xRatio += 5;
+                yRatio -= 5;
+                count++;
               });
             },
             child: Container(
               color: Color.fromARGB(255, 216, 139, 139),
-              height: x_ratio,
+              height: xRatio,
+              child: Center(child: Text('$count')),
             ),
           ),
           GestureDetector(
             onTap: () {
               setState(() {
-                x_ratio--;
-                y_ratio++;
+                xRatio -= 5;
+                yRatio += 5;
+                count++;
               });
             },
             child: Container(
               color: Color.fromARGB(255, 158, 231, 215),
-              height: y_ratio,
+              height: yRatio,
             ),
           ),
         ],
