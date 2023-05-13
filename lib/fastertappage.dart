cd57cb24673859179d19de61bd1394e0ea894f52
charan <charan.reings125@gmail.com>
2023-05-13 23:15:41 +0530

    show dialog added

--- lib/fastertappage.dart
+++ lib/fastertappage.dart
@@ -4,6 +4,10 @@ import 'dart:ui' as ui;
 
 class FasterTapPage extends State<FasterTapState> {
   int count = 0;
+  late double xRatio = tall(context) / 2;
+  late double yRatio = tall(context) / 2;
+  int step = 40;
+  bool done = false;
   // double height = 0;
   // double height = ui.window.physicalSize.height;
 
@@ -12,41 +16,78 @@ class FasterTapPage extends State<FasterTapState> {
     return height;
   }
 
-  late double xRatio = tall(context) / 2;
-  late double yRatio = tall(context) / 2;
-  int step = 20;
+  void checkHeight(double x, double y) {
+    if (x <= 30 || y <= 30) {
+      setState(() {
+        done = true;
+        if (done) {
+          showDialog(
+            barrierDismissible: false,
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('You won'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            done = false;
+
+                            xRatio = tall(context) / 2;
+                            yRatio = tall(context) / 2;
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: Text('Done'))
+                  ],
+                );
+              });
+        }
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Column(
         children: [
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                xRatio += step;
-                yRatio -= step;
-                count++;
-              });
-            },
-            child: Container(
-              color: Color.fromARGB(255, 216, 139, 139),
-              height: xRatio,
-              child: Center(child: Text('${xRatio.toInt()}')),
+          AbsorbPointer(
+            absorbing: done,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  xRatio += step;
+                  yRatio -= step;
+                  checkHeight(xRatio, yRatio);
+                  count++;
+                });
+              },
+              child: Container(
+                color: Color.fromARGB(255, 42, 189, 132),
+                height: xRatio,
+                // child: Center(child: Text('${xRatio.toInt()}')),
+              ),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                xRatio -= step;
-                yRatio += step;
-                count++;
-              });
-            },
-            child: Container(
-              color: Color.fromARGB(255, 158, 231, 215),
-              height: yRatio,
-              child: Center(child: Text('${yRatio.toInt()}')),
+          AbsorbPointer(
+            absorbing: done,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  xRatio -= step;
+                  yRatio += step;
+                  checkHeight(xRatio, yRatio);
+                  count++;
+                });
+              },
+              child: Container(
+                color: Color.fromARGB(255, 168, 98, 235),
+                height: yRatio,
+                // child: Center(child: Text('${yRatio.toInt()}')),
+              ),
             ),
           ),
         ],
