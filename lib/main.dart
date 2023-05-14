0b55a12854c9beed3fac72afd41f2569c69ac70b
charan <charan.reings125@gmail.com>
2023-05-14 08:38:58 +0530

    material3 support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:faster_tap/fastertappage.dart';
-import 'package:faster_tap/variables.dart';
 
 void main(List<String> args) {
-  runApp(FasterTap());
+  runApp(const FasterTap());
   
 }
 
 class FasterTap extends StatelessWidget{
-  FasterTap({super.key});
+  const FasterTap({super.key});
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      
       theme: ThemeData(
-          colorSchemeSeed: Color.fromARGB(255, 205, 141, 211), useMaterial3: true),
+          colorSchemeSeed: const Color.fromARGB(255, 205, 141, 211), useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: FasterTapState(),
+      home: const FasterTapState(),
     );
   }
 }
 
 class FasterTapState extends StatefulWidget{
-  FasterTapState({super.key});
+  const FasterTapState({super.key});
 
   @override
   State<FasterTapState> createState() => FasterTapPage();
