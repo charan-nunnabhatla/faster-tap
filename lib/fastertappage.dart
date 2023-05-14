cf4390ca02db86bb2c43b33475c0fd8c291294f3
charan <charan.reings125@gmail.com>
2023-05-14 08:38:06 +0530

    added lables

--- lib/fastertappage.dart
+++ lib/fastertappage.dart
@@ -1,13 +1,14 @@
 import 'package:faster_tap/main.dart';
 import 'package:flutter/material.dart';
-import 'dart:ui' as ui;
 
 class FasterTapPage extends State<FasterTapState> {
   int count = 0;
   late double xRatio = tall(context) / 2;
   late double yRatio = tall(context) / 2;
-  int step = 40;
+  int step = 10;
   bool done = false;
+  bool xWon = false;
+  bool yWon = false;
   // double height = 0;
   // double height = ui.window.physicalSize.height;
 
@@ -17,16 +18,22 @@ class FasterTapPage extends State<FasterTapState> {
   }
 
   void checkHeight(double x, double y) {
+    if (x > y) {
+      xWon = true;
+    } else {
+      yWon = true;
+    }
+
     if (x <= 30 || y <= 30) {
       setState(() {
         done = true;
         if (done) {
           showDialog(
-            barrierDismissible: false,
+              barrierDismissible: false,
               context: context,
               builder: (BuildContext context) {
                 return AlertDialog(
-                  title: Text('You won'),
+                  title: xWon ? const Text('Player 1 Won') : const Text('Player 2 Won'),
                   actions: [
                     TextButton(
                         onPressed: () {
@@ -38,7 +45,7 @@ class FasterTapPage extends State<FasterTapState> {
                           });
                           Navigator.pop(context);
                         },
-                        child: Text('Done'))
+                        child: const Text('Done'))
                   ],
                 );
               });
@@ -46,7 +53,6 @@ class FasterTapPage extends State<FasterTapState> {
       });
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -66,9 +72,20 @@ class FasterTapPage extends State<FasterTapState> {
                 });
               },
               child: Container(
-                color: Color.fromARGB(255, 42, 189, 132),
+                color: Color.fromARGB(255, 115, 207, 172),
                 height: xRatio,
-                // child: Center(child: Text('${xRatio.toInt()}')),
+                child: const Center(
+                    child: RotatedBox(
+                  quarterTurns: 2,
+                  child: Text(
+                    'Player 1',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'arial',
+                        color: Color.fromARGB(255, 120, 98, 182)),
+                  ),
+                )),
               ),
             ),
           ),
@@ -84,9 +101,17 @@ class FasterTapPage extends State<FasterTapState> {
                 });
               },
               child: Container(
-                color: Color.fromARGB(255, 168, 98, 235),
+                color: Color.fromARGB(255, 183, 145, 219),
                 height: yRatio,
-                // child: Center(child: Text('${yRatio.toInt()}')),
+                child: const Center(
+                    child: Text(
+                  'Player 2',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'arial',
+                      color: Color.fromARGB(255, 211, 24, 218)),
+                )),
               ),
             ),
           ),
